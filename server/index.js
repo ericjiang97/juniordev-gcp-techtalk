f81895c92fc0b0cccce0a26f8d65af5471c1e132
Eric Jiang <ericjiang97@gmail.com>
2018-08-27 10:58:47 +1000

    added gh user req

--- server/index.js
+++ server/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
 const APP_PORT = process.env.port || 8080;
+const axios = require('axios')
 
 const exampleRouter = (req, res) => {
   res.send('hello world!');
 }
-app.get('/', (req, res) => res.send('Hello from Google App Engine!'));
+app.get('/', (req, res) => res.send('Hello JuniorDev, from Google App Engine!'));
 app.use('/test', exampleRouter);
 app.get('/teapot', (req, res) => {
-  res.status(418).send("Hello I'm a teapot running on Node Standard GAE");
+  res.status(418).send("Hello JuniorDev, I'm a teapot running on Node Standard GAE");
+});
+
+
+app.get('/users/:username', (req, res) => {
+  const { username } = req.param
+  axios.get("https://api.github.com/users/" + username)
+    .then(resp => {
+      if(resp.status === 200){
+        res.send(resp.data)
+      } else {
+        res.status(500).send("Internal server error")
+      }
+    })
 });
 
 app.listen(APP_PORT, () =>
